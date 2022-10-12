2df24454b14f7ba0f5b7c071148051f394d64be1
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-10-12 14:57:15 -0300

    Fontes, Loading, StatusBar

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,8 +1,23 @@
+import { StatusBar } from "react-native";
 import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components";
+import theme from "@theme/index";
+
+import { Loading } from "@components/Loading";
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 export default function App() {
+const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
-    <Groups/>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+      barStyle='light-content'
+      backgroundColor="transparent"
+      translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading/> }
+    </ThemeProvider>
   );
 }
-
--- igniteteams/src/screens/Groups/styles.ts
+++ igniteteams/src/screens/Groups/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
+import theme from '@theme/index';
 
 export const Container = styled.View`
 flex: 1;
+background-color: ${({theme})=> theme.COLORS.GRAY_600};
+padding: 24px;
 `;
