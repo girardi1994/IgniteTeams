3a32eeaf46571483c3dd38abb5be6c7d30da677e
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-10-14 21:53:19 -0300

    Listando grupos

--- igniteteams/src/components/GroupCard/index.tsx
+++ igniteteams/src/components/GroupCard/index.tsx
@@ -8,7 +8,7 @@ import {
 } from './styles';
 
 type Props = TouchableOpacityProps && {
-  title: string
+  title: string,
 }
 
 export function GroupCard({title, ...rest}: Props){
--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 import React from 'react';
 
 import {
@@ -8,6 +12,8 @@ import {
 } from './styles';
 
 export function Groups(){
+const [groups, setGroups] = useState<string[]>([]);
+
    return (
      <Container>
       <Header/>
@@ -15,8 +21,19 @@ export function Groups(){
       title='Turmas'
       subtitle='Jogue com sua turma'
       />
-      <GroupCard
-      title= 'Galera do ignite'
+      <FlatList
+      data={groups}
+      keyExtractor={item => item}
+       renderItem={({item})=> (
+       <GroupCard 
+       title={item}
+       />
+   )}
+   contentContainerStyle={groups.length === 0 && {flex: 1}}
+   ListEmptyComponent={()=> (
+   <ListEmpty 
+   message='Que tal cadastrar a primeira turma?'/>
+   )}
       />
      </Container>
   );
