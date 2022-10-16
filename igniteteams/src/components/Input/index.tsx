7b829ef39af759adfd7bc30121510a08fc1a6d35
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-10-16 14:40:13 -0300

    Placeholder

--- igniteteams/src/components/Input/index.tsx
+++ igniteteams/src/components/Input/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
 
 import { Container } from "./styles";
 
 export function Input({ ...rest }: TextInputProps) {
-  return <Container {...rest} />;
+  const { COLORS } = useTheme();
+
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 }
--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -1,34 +1,26 @@
-import { Button } from '@components/Button';
-import { Header } from '@components/Header';
-import { HighLight } from '@components/HighLight';
-import { Input } from '@components/Input';
-import React from 'react';
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { Input } from "@components/Input";
+import React from "react";
 
-import {
-  Container,
-  Content,
-  Icon,
-} from './styles';
+import { Container, Content, Icon } from "./styles";
 
-export function NewGroup(){
-   return (
-     <Container>
+export function NewGroup() {
+  return (
+    <Container>
       <Header showBackButton />
 
       <Content>
-        <Icon/>
+        <Icon />
 
         <HighLight
-        title="Nova turma"
-        subtitle="Crie a turma para adicionar as pessoas"
+          title="Nova turma"
+          subtitle="Crie a turma para adicionar as pessoas"
         />
-        <Input/>
-        <Button
-        title="Criar"
-        style={{marginTop: 20}}
-        />
+        <Input placeholder="Nome da turma" />
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
-
-     </Container>
+    </Container>
   );
 }
