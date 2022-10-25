23962a9b51ac136c16c11e78bd4313774b4d5061
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-10-24 22:44:13 -0300

    ajustes

--- igniteteams/src/components/GroupCard/index.tsx
+++ igniteteams/src/components/GroupCard/index.tsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import { TouchableOpacityProps } from 'react-native';
+import React from "react";
+import { TouchableOpacityProps } from "react-native";
 
-import {
-  Container,
-  Title,
-  Icon
-} from './styles';
+import { Container, Title, Icon } from "./styles";
 
 type Props = TouchableOpacityProps & {
-  title: string,
-}
+  title: string;
+};
 
-export function GroupCard({title, ...rest}: Props){
-
-   return (
-     <Container>
-      <Icon/>
+export function GroupCard({ title, ...rest }: Props) {
+  return (
+    <Container {...rest}>
+      <Icon />
       <Title>{title}</Title>
-     </Container>
+    </Container>
   );
 }
--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,69 +1,58 @@
-import { useState, useEffect, useCallback } from 'react';
-import { FlatList } from 'react-native';
-import {useNavigation} from '@react-navigation/native'
+import { useState, useEffect, useCallback } from "react";
+import { FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import { Header } from '@components/Header';
-import { HighLight } from '@components/HighLight';
-import { GroupCard } from '@components/GroupCard';
-import { ListEmpty } from '@components/ListEmpty';
-import { Button } from '@components/Button';
-import { GroupsGetAll} from "../../Storage/group/groupsGetAll"
-import React from 'react';
+import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
+import { GroupsGetAll } from "../../Storage/group/groupsGetAll";
+import React from "react";
 
-import {
-  Container
-} from './styles';
+import { Container } from "./styles";
 
-export function Groups(){
-const [groups, setGroups] = useState<string[]>([]);
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
 
-const navigation = useNavigation();
+  const navigation = useNavigation();
 
-function handleNewGroup(){
-navigation.navigate("new")
-}
-async function fetchGroups(){
-  try {
-   const data = await GroupsGetAll();
-   setGroups(data);
-  } catch (error) {
-    
+  function handleNewGroup() {
+    navigation.navigate("new");
   }
-}
-function handleOpenGroup(group: string){
-navigation.navigate('players', {group});
-}
-useEffect(useCallback(()=>{
-  fetchGroups();
+  async function fetchGroups() {
+    try {
+      const data = await GroupsGetAll();
+      setGroups(data);
+    } catch (error) {}
+  }
+  
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
 
-},[]));
+  useEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
-   return (
-     <Container>
-      <Header/>
-      <HighLight
-      title='Turmas'
-      subtitle='Jogue com sua turma'
-      />
+  return (
+    <Container>
+      <Header />
+      <HighLight title="Turmas" subtitle="Jogue com sua turma" />
       <FlatList
-      data={groups}
-      keyExtractor={item => item}
-       renderItem={({item})=> (
-       <GroupCard 
-       title={item}
-       onPress={()=>handleOpenGroup(item)}
-       />
-   )}
-   contentContainerStyle={groups.length === 0 && {flex: 1}}
-   ListEmptyComponent={()=> (
-   <ListEmpty 
-   message='Que tal cadastrar a primeira turma?'/>
-   )}
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
-      <Button
-      title='Criar nova turma'
-      onPress={handleNewGroup}
-      />
-     </Container>
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
+    </Container>
   );
 }
